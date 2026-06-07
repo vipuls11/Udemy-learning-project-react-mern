e92e78978ec74f50b1cb3823d4557916791f0755
vipuls11 <vipulvishwakrma161@gmail.com>
2026-06-07 16:58:37 +0530

    Ui-Enhancement

--- src/shared/components/FormElements/Button.jsx
+++ src/shared/components/FormElements/Button.jsx
@@ -3,22 +3,28 @@ import { Link } from 'react-router-dom';
 import './Button.css'
 
 const Button = (props) => {
+    const classes = [
+        'button',
+        `button--${props.size || 'default'}`,
+        props.inverse ? 'button--inverse' : '',
+        props.danger ? 'button--danger' : ''
+    ].filter(Boolean).join(' ');
 
     if (props.href) {
         return (
-            <a className={`button button--${props.size || 'defaults'} ${props.inverse && ' button--inverse'} ${props.danger && 'button--danger'}`} href={props.href}>{props.children}</a>
+            <a className={classes} href={props.href}>{props.children}</a>
         );
     }
     if (props.to) {
         return (
-            <Link to={props.to} exact={props.exact} className={`button button--${props.size || 'defaults'} ${props.inverse && ' button--inverse'} ${props.danger && 'button--danger'}`}>
+            <Link to={props.to} exact={props.exact} className={classes}>
                 {props.children}
             </Link>
         );
     }
 
     return (
-        <button className={`button button--${props.size || 'defaults'} ${props.inverse && ' button--inverse'} ${props.danger && 'button--danger'}`}
+        <button className={classes}
             type={props.type}
             onClick={props.onClick}
             disabled={props.disabled}>
